a75db77e9420a01de71fb50adf760c772480d9b8
Peter Liang <sholfen@gmail.com>
2023-08-22 12:47:08 +0800

    first upload

---
+++ QRCodeGeneratorWebAP/Data/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace QRCodeGeneratorWebAP.Data
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}//https://sholfen.github.io/dotnet/2023/04/05/qr-code.html
