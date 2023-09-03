752a4b1aa82b8116d55b1cade0fc0d7beae8d150
Peter Liang <sholfen@gmail.com>
2023-09-03 14:47:09 +0800

    add localstorage function

---
+++ QRCodeGeneratorWebAP/Data/QRCodeData.cs
@@ -0,0 +1,9 @@
+﻿namespace QRCodeGeneratorWebAP.Data
+{
+    public class QRCodeData
+    {
+        public readonly static string KEY = "QRCODE";
+        public string URL { get; set; } = string.Empty;
+        public string ImagStr { get; set; } = string.Empty;
+    }
+}
--- QRCodeGeneratorWebAP/Program.cs
+++ QRCodeGeneratorWebAP/Program.cs
@@ -1,4 +1,5 @@
 using BlazorBootstrap;
+using Blazored.LocalStorage;
 using QRCodeGeneratorWebAP.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,7 @@ builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddSingleton<QRCodeAppService>();
 builder.Services.AddBlazorBootstrap();
+builder.Services.AddBlazoredLocalStorage();
 
 var app = builder.Build();
 
